5cd9c87f3a8e570ffeabe4815c39f7308f6bb5b1
Obieli <dum_obieli@yahoo.com>
2022-07-22 18:37:52 +0100

    step-17

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:worldie/constants/routes.dart';
 import 'package:worldie/services/auth/auth_service.dart';
 import 'package:worldie/views/login_view.dart';
-import 'package:worldie/views/notes/new_note_view.dart';
+import 'package:worldie/views/notes/create_update_note_view.dart';
 import 'package:worldie/views/notes/notes_view.dart';
 import 'package:worldie/views/register_view.dart';
 import 'package:worldie/views/verify_email_view.dart';
@@ -21,7 +21,7 @@ void main() {
         registerRoute: (context) => const RegisterView(),
         notesRoute: (context) => const NotesView(),
         verifyEmailRoute: (context) => const VerifyEmailView(),
-        newNoteRoute: (context) => const NewNoteView(),
+        createOrUpdateNoteRoute: (context) => const CreateUpdateNoteView(),
       },
     ),
   );
